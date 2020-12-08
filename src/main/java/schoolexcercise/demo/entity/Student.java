607359350479bf6abf6859f7558d482523e84846
Kamil Gaborek <BFK678@motorolasolutions.com>
2020-12-08 13:57:31 +0100

    added student entity, simpe api, start class

---
+++ src/main/java/schoolexcercise/demo/entity/Student.java
@@ -0,0 +1,40 @@
+package schoolexcercise.demo.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    private String addres;
+
+    public Student(String name, String addres) {
+        this.name = name;
+        this.addres = addres;
+    }
+
+    public Student(){}
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddres() {
+        return addres;
+    }
+
+    public void setAddres(String addres) {
+        this.addres = addres;
+    }
+}
