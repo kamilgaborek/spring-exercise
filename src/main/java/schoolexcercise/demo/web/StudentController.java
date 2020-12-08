680249343a8d72ca6750711aeb5b9fe5252d207b
Kamil Gaborek <BFK678@motorolasolutions.com>
2020-12-08 14:52:47 +0100

    security configuration, added roles and accesses to student api

--- src/main/java/schoolexcercise/demo/web/StudentController.java
+++ src/main/java/schoolexcercise/demo/web/StudentController.java
@@ -2,6 +2,8 @@ package schoolexcercise.demo.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import schoolexcercise.demo.data.StudentRepository;
@@ -21,5 +23,10 @@ public class StudentController {
     @GetMapping("/all")
     private Iterable<Student> getAll(){
         return studentRepository.findAll();
+    }
+
+    @PostMapping
+    public Student save(@RequestBody Student student){
+        return studentRepository.save(student);
     }
 }
